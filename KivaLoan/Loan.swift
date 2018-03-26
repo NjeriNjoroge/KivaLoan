8af54536396c81a5d0533767e556816fc0bf0d6a
NjeriNjoroge <wanjorogegrace@gmail.com>
2018-03-26 15:55:47 +0300

    app structure before Codable, Decodable

---
+++ KivaLoan/Loan.swift
@@ -0,0 +1,16 @@
+//
+//  Loan.swift
+//  KivaLoan
+//
+//  Created by Grace on 26/03/2018.
+//  Copyright © 2018 AppCoda. All rights reserved.
+//
+
+import Foundation
+
+struct Loan {
+    var name: String = ""
+    var country: String = ""
+    var use: String = ""
+    var amount: Int = 0
+}
